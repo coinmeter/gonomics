f4142ac000d055c13d3ed63e7ab9ec3d3806dd76
Dariush Abbasi <dariushem@yahoo.com>
2022-02-27 19:37:58 +0330

    add nomics base api definitions

--- nomics.go
+++ nomics.go
@@ -1,5 +1,9 @@
 package nomics
 
+import (
+	"net/http"
+)
+
 const (
 	APIBase = "https://api.nomics.com/v1"
 
@@ -14,3 +18,47 @@ const (
 	FiatExchangeRates        = "exchange-rates"
 	FiatExchangeRatesHistory = "exchange-rates/history"
 )
+
+// Noimcs defines the supported subset of the nomics API.
+type Nomics interface {
+	APIBase() string
+	APIKey() string
+	Client() *http.Client
+	SetClient(client *http.Client)
+}
+
+// NomicsBase bundles data needed by a large number of methods in order to interact with the nomics API.
+type NomicsBase struct {
+	apiBase string
+	apiKey  string
+	client  *http.Client
+}
+
+// NewNomics  creates a new client instance.
+func NewNomics(apiKey string) *NomicsBase {
+	return &NomicsBase{
+		apiBase: APIBase,
+		apiKey:  apiKey,
+		client:  http.DefaultClient,
+	}
+}
+
+// APIBase returns the API Base URL configured for this client.
+func (nm *NomicsBase) APIBase() string {
+	return nm.apiBase
+}
+
+// ApiKey returns the API key configured for this client.
+func (nm *NomicsBase) APIKey() string {
+	return nm.apiKey
+}
+
+// Client returns the HTTP client configured for this client.
+func (nm *NomicsBase) Client() *http.Client {
+	return nm.client
+}
+
+// SetClient updates the HTTP client for this client.
+func (nm *NomicsBase) SetClient(c *http.Client) {
+	nm.client = c
+}
