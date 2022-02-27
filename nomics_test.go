3a5230f064ff82a19a5d8381b52f21298721d5f0
Dariush Abbasi <dariushem@yahoo.com>
2022-02-27 20:25:20 +0330

    start using testify for testing

--- nomics_test.go
+++ nomics_test.go
@@ -1,14 +1,14 @@
 package nomics
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewNomics(t *testing.T) {
 	t_apikey := "notvalidkey"
 	nclient := NewNomics(t_apikey)
 
-	if nclient.APIKey() != t_apikey {
-		t.Errorf("%s FAILED. Got %s, expected %s\n", "TestNewNomics", nclient.APIKey(), t_apikey)
-	} else {
-		t.Logf("%s PASSED. Got %s, expected %s\n", "TestNewNomics", nclient.APIKey(), t_apikey)
-	}
+	assert.Equal(t, nclient.APIKey(), t_apikey, "they should be equal.")
 }
