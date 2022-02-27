fbd549c28342e028a4ca79c1ea4ea436aed5dada
Dariush Abbasi <dariushem@yahoo.com>
2022-02-27 19:12:33 +0330

    add nomics base api routes constants

---
+++ nomics.go
@@ -0,0 +1,16 @@
+package nomics
+
+const (
+	APIBase = "https://api.nomics.com/v1"
+
+	CurrenciesTicker   = "currencies/ticker"
+	CurrenciesMetadata = "currencies"
+
+	Markets          = "markets"
+	MarketCapHistory = "market-cap/history"
+
+	GlobalVolumeHistory = "volume/history"
+
+	FiatExchangeRates        = "exchange-rates"
+	FiatExchangeRatesHistory = "exchange-rates/history"
+)
